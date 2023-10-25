9de0e932ea02a8ff486bdac567bf808c9d84d035
Marcos <91590873+MarcosDeLaMarta@users.noreply.github.com>
2023-10-25 21:23:58 +0200

    Add files via upload

---
+++ scriptApache.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt upgrade
+sudo apt install -y apache2
+sudo apt install -y php
+sudo apt install -y git
+git clone https://github.com/josejuansanchez/iaw-practica-lamp.git
+
---
+++ scriptSql.sh
@@ -0,0 +1,4 @@
+sudo apt update
+sudo apt upgrade
+sudo apt install -y mariadb-server
+git clone https://github.com/josejuansanchez/iaw-practica-lamp.git
